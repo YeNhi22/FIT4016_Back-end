8fec17668d9eafc6558574768a7e1c78a607b46e
YenNhi22 <yennhinguyenvu.22@gmail.com>
2025-04-07 23:14:27 +0700

    Add initital project files

---
+++ Bai1Lab1.3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Một đơn vị sản xuất gồm có các cán bộ là công nhân, kỹ sư, nhân viên.
++ Mỗi cán bộ cần quản lý các thuộc tính: Họ tên, năm sinh, giới tính, địa chỉ
++ Các công nhân cần quản lý: Bậc (công nhân bậc 3/7, bậc 4/7 ...)
++ Các kỹ sư cần quản lý: Ngành đào tạo
++ Các nhân viên phục vụ cần quản lý thông tin: công việc
+1. Xây dựng các lớp NhanVien, CongNhan, KySu kế thừa từ lớp CanBo
+2. Xây dựng các phương thức nhập, hiển thị thông tin và kiểm tra về các thuộc tính của các
+lớp.
+3. Xây dựng lớp QLCB cài đặt các phương thức thực hiện các chức sau:
+- Nhập thông tin mới cho cán bộ (Hỏi người dùng muốn nhập cho: công nhân, kỹ sư
+hay nhân viên và nhập đúng thông tin cho đối tượng đó).
+- Tìm kiếm theo họ tên
+- Hiển thị thông tin về danh sách các cán bộ
+- Thoát khỏi chương trình.*/
+
