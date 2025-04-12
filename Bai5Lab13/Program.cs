ef27c8ab81d5eafc59b516436f5470901436ab8b
YenNhi22 <yennhinguyenvu.22@gmail.com>
2025-04-12 20:02:10 +0700

    Add initital project files

---
+++ Bai5Lab13/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Để quản lý khách hàng đến thuê phòng của một khách sạn, người ta cần quản lý nhữngthông tin sau:
+    - Số ngày trọ, loại phòng trọ, giá phòng, và các thông tin cá nhân về mỗi khách trọ.
+    - Với mỗi cá nhân, người ta cần quản lý các thông tin : Họ và tên, năm sinh, số chứng    minh thư nhân dân.
+1. Hãy xây dựng lớp Nguoi để quản lý thông tin cá nhân về mỗi cá nhân.
+2. Xây dựng lớp KhachSan để quản lý các thông tin về khách thuê phòng.
+3. Viết các phương thức : nhập, hiển thị các thông tin về mỗi khách thuê phòng.
+4. Cài đặt chương trình thực hiện các công việc sau:
+    - Nhập vào một dãy gồm n khách trọ ( n - nhập từ bàn phím)
+    - Hiển thị ra màn hình thông tin về các cá nhân hiện đang trọ ở khách sạn đó.
+    - Tìm kiếm thông tin về những khách thuê phòng theo họ và tên.
+    - Tính tiền cho khách hàng khi thanh toán trả phòng
+    - Thoát khỏi chương trình*/
