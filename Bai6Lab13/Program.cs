b23952b20df60667534dd181c0aad95247f0cb82
YenNhi22 <yennhinguyenvu.22@gmail.com>
2025-04-12 20:21:02 +0700

    Add initital project files

---
+++ Bai6Lab13/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Để quản lý hồ sơ học sinh của trường THPT, người ta cần quản lý những thông tin như
+sau:
+    - Các thông tin về : lớp, khoá học, kỳ học, và các thông tin cá nhân của mỗi học sinh.
+    - Với mỗi học sinh, các thông tin cá nhân cần quản lý gồm có: Họ và tên, tuổi, năm
+    sinh, quê quán, giới tính.
+1. Hãy xây dựng lớp Nguoi để quản lý các thông tin cá nhân của mỗi học sinh.
+2. Xây dựng lớp HSHocSinh (hồ sơ học sinh) để lý các thông tin về mỗi học sinh.
+3. Xây dựng các phương thức : nhập, hiển thị các thông tin về hồ sơ cá nhân của mỗi học sinh.
+4. Cài đặt chương trình thực hiện các công việc sau:
+    - Nhập vào một danh sách gồm n học sinh ( n- nhập từ bàn phím)
+    - Hiển thị ra màn hình tất cả những học sinh nữ và sinh năm 1985.
+    - Tìm kiếm học sinh theo quê quán.
+    - Thoát khỏi chương trình*/
