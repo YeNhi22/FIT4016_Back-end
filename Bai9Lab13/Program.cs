d1cfc10348dec7ddf1e6fd3f62912cdbdd8be523
YenNhi22 <yennhinguyenvu.22@gmail.com>
2025-04-12 22:49:36 +0700

    Add initital project files

---
+++ Bai9Lab13/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Để quản lý các biên lai thu tiền điện, người ta cần các thông tin như sau:
+    - Với mỗi biên lai, có các thông tin sau: thông tin về hộ sử dụng điện, chỉ số cũ, chỉ số
+    mới, số tiền phải trả của mỗi hộ sử dụng điện
+    - Các thông tin riêng của mỗi hộ sử dụng điện gồm: Họ tên chủ hộ, số nhà, mã số công
+    tơ của hộ dân sử dụng điện.
+1. Hãy xây dựng lớp KhachHang để lưu trữ các thông tin riêng của mỗi hộ sử dụng điện.
+2. Xây dựng lớp BienLai để quản lý việc sử dụng và thanh toán tiền điện của các hộ dân.
+3. Xây dựng các phương thức nhập, và hiển thị thông tin riêng của mỗi hộ sử dụng điện.
+4. Cài đặt chương trình thực hiện các công việc sau:
+    + Nhập vào các thông tin cho N hộ sử dụng điện
+    + Hiển thị thông tin về các biên lai đã nhập
+    + Tính tiền điện phải trả cho mỗi hộ dân, nếu giả sử rằng tiền phải trả được tính theo
+    công thức sau:
+        Số điện Giá tiền
+        Dưới 50 số 1250 vnđ/1 số
+        Từ 50 đến dưới 100 số 1500 vnđ/1 số
+        Từ 100 số trở lên 2000 vnđ/1 số*/
