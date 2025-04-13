7d35c05a9a762fce810369add2c1e3b23d11a734
YenNhi22 <yennhinguyenvu.22@gmail.com>
2025-04-13 11:11:45 +0700

    Add initital project files

---
+++ Bai12Lab13/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Xây dựng lớp MaTran có các thuộc tính riêng như sau:
+    + Số dòng, số cột của ma trận
+    + Một mảng hai chiều để lưu trữ các phần tử của ma trận Hãy:
+1. Xây dựng các hàm tạo : MaTran(), maTran(int n, int m); (Toán tử tạo lập thứ hai để tạo ra
+ma trận có n dòng và m cột)
+2. Xây dựng các phương thức: Nhập vào và hiển thị một ma trận
+3. Xây dựng các phương thức tính tổng, hiệu và tích, thương của hai ma trận
+4. Cài đặt chương trình thực hiện : Nhập vào hai ma trận A và B cùng cấp, sau đó hỏi người
+dùng muốn thực hiện tác vụ nào:
+    a) Tính tổng hai ma trận;
+    b) Tính tích hai ma trận;
+    c) Tính hiệu hai ma trận;
+    d) Tính thương hai ma trận
+    Hiển thị kết quả ra màn hình.*/
