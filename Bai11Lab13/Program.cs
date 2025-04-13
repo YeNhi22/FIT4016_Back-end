223063c45e612da03483d57d8fc5d275ad42397c
YenNhi22 <yennhinguyenvu.22@gmail.com>
2025-04-13 11:07:37 +0700

    Add initital project files

---
+++ Bai11Lab13/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Xây dựng lớp SoPhuc có các thuộc tính riêng gồm: phanThuc, phanAo kiểu double
+1. Xây dựng các hàm tạo như sau: SoPhuc(), SoPhuc(double a, double b)
+2. Xây dựng các phương thức:+ Nhập vào một số phức
+    + Hiển thị một số phức
+    + Cộng hai số phức.
+    + Nhân hai số phức
+    + Chia hai số phức.
+3. Cài đặt chương trình thực hiện : Nhập vào hai số phức A và B, sau đó hỏi người dùng muốn
+thực hiện tác vụ nào:
+    a) Tính tổng hai số phức;
+    b) Tính hiệu hai số phức;
+    c) Tính tích hai số phức;
+    d) Tính thương hai số phức;
+    Rồi hiển thị kết quả ra màn hình*/
---
+++ Bai11Lab13/SoPhuc.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai11Lab13
+{
+    internal class SoPhuc
+    {
+        public double PhanThuc { get; set; }
+        public double PhanAo { get; set; }
+
+        public SoPhuc()
+        {
+            PhanThuc = 0;
+            PhanAo = 0;
+        }
+
+        public SoPhuc(double a, double b)
+        {
+            PhanThuc = a;
+            PhanAo = b;
+        }
+
+        public void Nhap()
+        {
+            Console.Write("Nhập phần thực: ");
+            PhanThuc = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhập phần ảo: ");
+            PhanAo = double.Parse(Console.ReadLine());
+        }
+
+        public void HienThi()
+        {
+            string dau = PhanAo >= 0 ? "+" : "-";
+            Console.WriteLine($"{PhanThuc} {dau} {Math.Abs(PhanAo)}i");
+        }
+
+        public SoPhuc Cong(SoPhuc sp)
+        {
+            return new SoPhuc(this.PhanThuc + sp.PhanThuc, this.PhanAo + sp.PhanAo);
+        }
+
+        public SoPhuc Tru(SoPhuc sp)
+        {
+            return new SoPhuc(this.PhanThuc - sp.PhanThuc, this.PhanAo - sp.PhanAo);
+        }
+
+        public SoPhuc Nhan(SoPhuc sp)
+        {
+            double a = this.PhanThuc * sp.PhanThuc - this.PhanAo * sp.PhanAo;
+            double b = this.PhanThuc * sp.PhanAo + this.PhanAo * sp.PhanThuc;
+            return new SoPhuc(a, b);
+        }
+
+        public SoPhuc Chia(SoPhuc sp)
+        {
+            double mau = sp.PhanThuc * sp.PhanThuc + sp.PhanAo * sp.PhanAo;
+            if (mau == 0)
+                throw new DivideByZeroException("Không thể chia cho số phức có cả phần thực và ảo bằng 0.");
+
+            double a = (this.PhanThuc * sp.PhanThuc + this.PhanAo * sp.PhanAo) / mau;
+            double b = (this.PhanAo * sp.PhanThuc - this.PhanThuc * sp.PhanAo) / mau;
+            return new SoPhuc(a, b);
+        }
+    }
+}
