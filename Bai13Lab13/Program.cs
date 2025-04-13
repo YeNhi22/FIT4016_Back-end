5c4632576d2ed546e6041e321b27d1a944a408b4
YenNhi22 <yennhinguyenvu.22@gmail.com>
2025-04-13 11:19:12 +0700

    Add initital project files

---
+++ Bai13Lab13/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Một công ty được giao nhiệm vụ quản lý các phương tiện giao thông gồm các loại: ô
+tô, xe máy, xe tải.
+    + mỗi loại phương tiện giao thông cần quản lý: Hãng sản xuất, năm sản xuất, giá bán
+    và màu.
+    + Các ô tô cần quản lý: số chỗ ngồi, kiểu động cơ+ Xe máy cần quản lý: công suất
+    + Xe tải cần quản lý: trọng tải.
+1. Xây dựng các lớp XeTai, XeMay, OTo kế thừa từ lớp PTGT.
+2. Xây dựng các hàm để truy nhập, hiển thị và kiểm tra các thuộc tính của các lớp.
+3. Xây dựng lớp QLPTGT cài đặt các phương thức thực hiện các chức năng sau:
+    - Nhập đăng ký phương tiện
+    - Tìm phương tiện theo màu hoặc theo năm sản xuất.
+    - Kết thúc chương trình.*/
