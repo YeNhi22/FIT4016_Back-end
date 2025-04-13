d4d51a55a748cd62e5908ce9bb594c72cb4a27b9
YenNhi22 <yennhinguyenvu.22@gmail.com>
2025-04-13 11:00:28 +0700

    Add initital project files

---
+++ Bai10Lab13/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Để xử lý các văn bản, người ta xây dựng lớp VanBan có thuộc tính riêng là một xâu
+ký tự. Hãy:
+    1. Xây dựng các hàm tạo không có và có đối số như sau: VanBan(), VanBan(String st).
+    2. Xây dựng phương thức đếm số từ của một xâu.
+    3. Xây dựng phương thức đếm số ký tự H (không phân biệt chữ thường, chữ hoa) của xâu.
+    4. Chuẩn hoá một xâu theo tiêu chuẩn (Ở đầu và cuối của xâu không có ký tự trống, ở giữa
+    xâu không có hai ký tự trắng liền nhau).
+    5. Xây dựng một menu hỏi người sử dụng muốn thực hiện công việc gì (đếm từ, đếm số kí tự
+    H hãy chuẩn hóa sâu). Sau đó hiển thị kết quả ra màn hình.*/
